a722910f7f504205d05af310f826a3afdd9f73f7
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-04-15 21:14:19 +0700

    first commit

--- Struct2.cpp
+++ Struct2.cpp
@@ -17,3 +17,23 @@ struct mahasiswa
 
 int main(){
     mahasiswa mhs;
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan NAMA = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
+
+    
+
+    //menampilkan data struct
+    cout << endl;
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Desa = " << mhs.alamat.desa << endl;
+    cout << "Kota = " << mhs.alamat.kota << endl;
+
+}
