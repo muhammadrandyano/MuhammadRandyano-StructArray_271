5e9757b2817551120c48e8b78000976bf31c25bc
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-04-15 21:10:34 +0700

    first commit

---
+++ Srtuct.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa
+{
+    string nim;
+    string nama;
+    string alamat;
+};
+
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim = "20240140271";
+    mhs.nama = "Muhammad Randyano";
